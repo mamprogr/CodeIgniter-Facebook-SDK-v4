1cd52a424e479d6ff7c6246a2689bef2915fc4a2
mamprogr <mamprogr@gmail.com>
2014-12-10 03:24:53 +0200

    Init Facebook SDK v4.

---
+++ application/config/facebook.php
@@ -0,0 +1,16 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['facebook']['api_id'] = 'YOUR APP ID';
+
+$config['facebook']['app_secret'] = 'YOUR APP SECRET';
+
+$config['facebook']['redirect_url'] = 'http://www.yourwebsite.com/login';
+
+$config['facebook']['permissions'] = array(
+  'email',
+  'user_location',
+  'user_birthday'
+);
+
+/* End of file facebook.php */
+/* Location: ./application/config/facebook.php */
